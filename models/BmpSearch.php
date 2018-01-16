059939f540b8f717cf6532e1a1d775aa571b38d9
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2018-01-16 19:13:20 +0200

    Search model

--- models/BmpSearch.php
+++ models/BmpSearch.php
@@ -11,5 +11,32 @@ namespace app\models;
 
 class BmpSearch
 {
+    public $beginDate;
+    public $endDate;
+    public $beginTemperature;
+    public $endTemperature;
+    public $beginPressure;
+    public $endPressure;
+    public $beginAltitude;
+    public $endAltitude;
 
+    public function __construct($json = false) {
+        if ($json) {
+            if ($json->data)
+                $this->set($json->data, true);
+            else
+                $this->set($json, true);
+        }
+    }
+
+    public function set($data) {
+        foreach ($data AS $key => $value) {
+            if (is_array($value)) {
+                $sub = new JSONObject;
+                $sub->set($value);
+                $value = $sub;
+            }
+            $this->{$key} = $value;
+        }
+    }
 }
